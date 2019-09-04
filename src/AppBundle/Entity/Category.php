1c1a15ee1b6faa834660c9cf17e60bbfc692959b
laptop <user@user.com>
2019-09-04 14:39:01 +0200

    start

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    // ...
+
+    /**
+     * @ORM\OneToMany(targetEntity="Product", mappedBy="category")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
---
+++ src/AppBundle/Repository/ProductRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace AppBundle\Repository;
+
+
+class ProductRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
